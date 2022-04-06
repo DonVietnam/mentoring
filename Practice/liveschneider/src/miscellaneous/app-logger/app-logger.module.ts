f1fbe1c56befa8a4a45a0545994172046722fc47
Michel Schneider <54135398+MChuduk@users.noreply.github.com>
2022-04-06 14:34:03 +0300

    Delete Practice directory

--- Practice/liveschneider/src/miscellaneous/app-logger/app-logger.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AppLoggerService } from './app-logger.service';
-
-@Module({
-  exports: [AppLoggerService],
-  providers: [AppLoggerService],
-})
-export class AppLoggerModule {}
